83e54aad676eaa120634882e3355443b4ce5c885
Amadou Toure <amadouelcapotoure@gmail.com>
2025-04-10 00:17:39 -0400

    todo-handlers/Prefume.handler.go:add a condition to reject request when the image already exist

---
+++ utils/Token.go
@@ -0,0 +1,11 @@
+package utils
+
+// import (
+// 	"go/token"
+// 	"github.com/golang-jwt/jwt/v5"
+// )
+
+// func GenerateTokens(id string, secret string){
+// 	token:=jwt.NewWithClaims(jwt.SigningMethodES256,claims)
+
+// }
