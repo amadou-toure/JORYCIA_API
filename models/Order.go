6d05c0504ca1a8bb75f0b2c9b6692243a873ca37
Amadou Toure <amadouelcapotoure@gmail.com>
2025-07-06 00:50:39 -0400

    now, check for the checkout status. if complete, create a new order object, copy the cart in order and clear cart #6

--- models/Order.go
+++ models/Order.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Order struct {
-	ID              string     `bson:"_id,omitempty" json:"id"`
+	ID              string     `bson:"_id" json:"id"`
 	UserID          string    `bson:"user_id,omitempty" json:"userId,omitempty"`
 	Items           []OrderItem`bson:"items" json:"items"`
 	Total           float64   `bson:"total" json:"total"`
 	CreatedAt       *time.Time `bson:"created_at" json:"createdAt"`
 	UpdatedAt       *time.Time `bson:"updated_at,omitempty" json:"updatedAt,omitempty"`
 	StripeSessionID *string    `bson:"stripe_session_id,omitempty" json:"stripeSessionId,omitempty"`
-	ShippingAddress string	 	`bson:"stripe_shipping_address,omitempty" json:"stripeShippingAddress"`
-	PaymentStatus   *string    `bson:"payment_status,omitempty" json:"paymentStatus,omitempty"`
+	ShippingAddress string	 	`bson:"stripe_shipping_address" json:"stripeShippingAddress"`
+	PaymentStatus   *string    `bson:"payment_status" json:"paymentStatus"`
 	Status          *string    `bson:"status" json:"status"`
 }
 
